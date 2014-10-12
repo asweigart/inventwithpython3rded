33233baf36c076dae94c23957c22d1b022759de4
Al Sweigart <asweigart@gmail.com>
2014-10-12 16:28:58 +0200

    Adding first Spanish source code files for Hello and Guess the Number.

---
+++ src/es/hello.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Este programa te saluda y pregunta por mi nombre.
+print('¡Hola Mundo!')
+print('Cuál es tu nombre?')
+miNombre = input()
+print('Es un placer conocerte, ' + miNombre)
