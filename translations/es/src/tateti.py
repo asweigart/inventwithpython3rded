8d435e8d38a6f5bbc356b44ef98d1f6f6bb843dc
Al Sweigart <asweigart@gmail.com>
2015-01-17 12:47:23 +1100

    Fixing spanish translation issues

--- translations/es/src/tateti.py
+++ translations/es/src/tateti.py
@@ -41,7 +41,7 @@ def quienComienza():
 
 def jugarDeNuevo():
     # Esta funcion devuelve True (Verdadero) si el jugador desea volver a jugar, de lo contrario devuelve False (Falso).
-    print('¿Deseas volver a jugar? (si/no)?')
+    print('¿Deseas volver a jugar? (sí/no)?')
     return input().lower().startswith('s')
 
 def hacerJugada(tablero, letra, jugada):
@@ -148,7 +148,7 @@ while True:
     juegoEnCurso = True
 
     while juegoEnCurso:
-        if turno == 'jugador':
+        if turno == 'El jugador':
             # Turno del jugador
             dibujarTablero(elTablero)
             jugada = obtenerJugadaJugador(elTablero)
